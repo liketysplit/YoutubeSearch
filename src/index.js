d77d93661a552e44b07bc707ba3856bd3998c3f9
liketysplit <rjboles2@gmail.com>
2018-09-04 11:54:01 -0400

    1.0 YoutTube API

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+const API_KEY = "AIzaSyD96qHmy3mUR_m8vTmbXSQdhTTavvjfp_A";
+ReactDOM.render( < App / > , document.getElementById('root'));
 registerServiceWorker();
