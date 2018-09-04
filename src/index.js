a14f9eee6587906f336a15b992899b5f98ec6339
liketysplit <rjboles2@gmail.com>
2018-09-04 12:12:24 -0400

    2.2 Adding functionality to the search bar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import SearchBar from "./components/search_bar";
+
 const API_KEY = "AIzaSyD96qHmy3mUR_m8vTmbXSQdhTTavvjfp_A";
-ReactDOM.render( < App / > , document.getElementById('root'));
-registerServiceWorker();
+
+const App = () => {
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
