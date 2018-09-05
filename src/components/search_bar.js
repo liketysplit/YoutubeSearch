960dabcbf9d07279baf058b06c55d54272023e82
liketysplit <rjboles2@gmail.com>
2018-09-05 14:09:39 -0400

    2.3 React State - SearchBar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,14 +1,26 @@
-import React, {
-    Component
-} from 'react';
+import React, { Component } from "react";
 
 class SearchBar extends React.Component {
-    render() {
-        return <input onChange = {
-            (event) => console.log(event.target.value)
-        }
-        />
-    }
+	constructor(props) {
+		super(props);
+
+		this.state = { term: "" };
+	}
+	render() {
+		return (
+			<div>
+				<input
+					value={this.state.term}
+					onChange={event =>
+						this.setState({
+							term: event.target.value
+						})
+					}
+				/>
+				Value of input: {this.state.term}{" "}
+			</div>
+		);
+	}
 }
 
 export default SearchBar;
